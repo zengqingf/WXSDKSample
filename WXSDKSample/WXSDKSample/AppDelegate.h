f5a6dbd97ad08e2b99f46e89f9d6a398e9ef28b2
zengqingfu-jx <jx@cengqingfudeMacBook-Pro.local>
2016-04-06 16:42:37 +0800

    update

---
+++ WXSDKSample/WXSDKSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WXSDKSample
+//
+//  Created by zengqingfu-jx on 16/4/6.
+//  Copyright © 2016年 zengqingfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SendMsgToWeChatViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) SendMsgToWeChatViewController *viewController;
+
+@end
+
---
+++ WXSDKSample/WXSDKSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WXSDKSample
+//
+//  Created by zengqingfu-jx on 16/4/6.
+//  Copyright © 2016年 zengqingfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
